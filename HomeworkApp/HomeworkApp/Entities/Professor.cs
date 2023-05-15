ddd373a64eb2a69432dd65aa1e1b90bada54d9d9
Serg <serg.albokrinov@gmail.com>
2023-05-15 21:04:33 +0200

    Commit

---
+++ HomeworkApp/HomeworkApp/Entities/Professor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkApp.Entities
+{
+    public class Professor : EntityBase
+    {
+        public string? Name { get; set; }
+
+        public override string ToString() => $"Id: {Id}, Name: {Name}";
+    }
+}
---
+++ HomeworkApp/HomeworkApp/Entities/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkApp.Entities
+{
+    public class Student : EntityBase
+    {
+        public string? FirstName { get; set; }
+        public override string ToString() => $"Id: {Id}, FirstName: {FirstName}";
+    }
+}
